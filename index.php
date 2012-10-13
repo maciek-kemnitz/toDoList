dbff8f68f3f63806a67e65ca6307e80cdf40fde3
maciek-kemnitz <maciek.kemnitz@gmail.com>
2012-10-13 13:20:10 +0200

    Curl

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
+$c = curl_init();
+curl_setopt_array($c, array (
+  CURLOPT_URL => 'https://basecamp.com/1758651/api/v1/projects.json',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_USERPWD => 'maciek.kemnitz:tiGGer987g',
+  
+  //CURLOPT_POSTFIELDS => json_encode(array("name" => "myproject")),
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array (
+    'User-Agent: Test (test@example.com)',
+    'Content-Type: application/json',
+  ),
+));
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+
+$data = curl_exec($c);
+$data = json_decode($data, true);
+print_r($data);
